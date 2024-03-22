0eb18e4f8559f16f3b8a8543fc3f1f25f9054879
Bruno Araujo <bruno.cadoma@gmail.com>
2024-03-22 14:56:34 -0300

    Projeto Spring inicializado

---
+++ dogspot/src/main/java/com/dogspot/dogspot/DogspotApplication.java
@@ -0,0 +1,13 @@
+package com.dogspot.dogspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogspotApplication.class, args);
+	}
+
+}
